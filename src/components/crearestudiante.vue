4dfeafe3bfbf9f647a9a2321a0f22d09c3ae2133
Daniiparedes <Basilio.paredes2020@twk.cl>
2020-10-31 20:26:11 -0300

    commit

---
+++ src/components/crearestudiante.vue
@@ -0,0 +1,63 @@
+<template>
+<div id = "Ingresar">
+  <form  target="_blank">
+    <h1><b> Ingrese Datos del Estudiante </b></h1>
+    <h2> <b> Complete Formulario </b> </h2>
+    <p> Nombre del Estudiante: <input type="text" name="nombrecompleto"></p>
+    <p>Nombre del Apoderado: <input type="text" name="nombrecompleto"></p>
+    <p>
+      Curso:
+      <select name="Curso">
+        <option>KinderA</option>
+        <option>KinderB</option>
+
+      </select>
+
+    </p>
+    <p>Curso: <input type="text" name="nombrecompleto"></p>
+    <p>Fecha de Nacimiento: <input type="Date" name="nombrecompleto"></p>
+    <p>
+      Sexo:<br>
+      <input type="checkbox" name="Masculino"> Masculino<br>
+      <input type="checkbox" name="Femenino"> Femenino<br>
+      <input type="checkbox" name="otres"> otres
+    </p>
+    <p>Domicilio: <input type="text" name="nombrecompleto"></p>
+    <p>Telefono: <input type="text" name="nombrecompleto"></p>
+    <p>Celular: <input type="text" name="nombrecompleto"></p>
+    <p>Domicilio: <input type="text" name="nombrecompleto"></p>
+    <p>Domicilio: <input type="text" name="nombrecompleto"></p>
+    <p>Domicilio: <input type="text" name="nombrecompleto"></p>
+
+    <p>
+      Observaciones:<br>
+      <textarea name="mensaje"></textarea>
+    </p>
+    <input class="btnGUARDAR" type="submit" value="Guardar">
+  </form>
+</div>
+  <div id="imgEstudiante">
+    <input type="file">
+  </div>
+</template>
+
+<script>
+export default {
+name: "crearestudiante"
+}
+</script>
+
+<style scoped>
+#Ingresar {
+  border:3px solid #000;
+  text-align: left;
+}
+p,h1 {
+  font-family: "Comic Sans MS";
+}
+ .btnGUARDAR{
+   sumbit-align: center;
+   font-family: "Comic Sans MS";
+ }
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,34 @@
 import Vue from 'vue'
 import App from './App.vue'
+//npm install --save vue-router
+import VueRouter from 'vue-router'; //Importamos librería para rutas
+import Login from './components/Login.vue'; //importamos nuestros componentes a utilizar
+import Home from './components/Home.vue';
+import crearestudiante from "@/components/crearestudiante";
+
+
+
 
 Vue.config.productionTip = false
 
+Vue.use(VueRouter); //Decimos a vue que vamos a utilizar la libreria instalada
+
+const routes = [  //Definimos los path y donde nos llevará cada uno
+  { path: '/',component:Login},
+  { path: '/Login/:id?', name: 'Login', component: Login },  //parámetro opcional (?), para que sea obligatorio se debe quitar el simbolo de pregunta
+  { path: '/Home', component: Home },
+  { path: '/crearestudiante', component: crearestudiante },
+
+
+]
+
+//Creamos el objeto vuerouter
+const router = new VueRouter({
+  routes, //le pasamos el array de rutas
+  mode: 'history' //para que no recargue la pagina al cambiar la ruta
+})
+
 new Vue({
+  router, //agremos el objeto router a la instancia de Vue para que sea global
   render: h => h(App),
 }).$mount('#app')
